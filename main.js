b9845bbcb2c8cc499990ac037abeb18eddd32bcf
Marco-Jan <marco.jan@gmx.de>
2023-08-01 13:02:04 +0200

    init commit

--- main.js
+++ main.js
@@ -1,110 +1,101 @@
-const GameBoard = () => {
-    let gameBoard = [];
-    let players = [];
-    let currentPlayerIndex = 0;
-    
-    class Player {
-        constructor(name, color) {
-            this.name = name;
-            this.color = color;
-            this.winnigs = 0;
-        }
+class Player {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+        this.winnings = 0;
+    }
+}
+
+class GameBoard {
+    constructor() {
+        this.gameBoard = [];
+        this.players = [];
+        this.currentPlayerIndex = 0;
+        this.initializeGameBoard();
     }
 
+    createElement(tag, attributes = {}, textContent = '') {
+        const element = document.createElement(tag);
+        Object.entries(attributes).forEach(([key, value]) => {
+            element.setAttribute(key, value);
+        });
+        element.textContent = textContent;
+        return element;
+    }
 
-    function initializeGameBoard() {
+    initializeGameBoard() {
         const boardElements = document.querySelector('.game');
-        const playerInfoElement = document.createElement('div');
-        const player1Label = document.createElement('label');
-        const player1Input = document.createElement('input');
-        player1Label.innerHTML = "Player 1: ";
-        player1Input.setAttribute('type', 'text');
-        playerInfoElement.classList.add('playerinfo', 'visible');
-        player1Input.id = 'name1';
-        boardElements.appendChild(playerInfoElement);
-        playerInfoElement.appendChild(player1Label);
-        playerInfoElement.appendChild(player1Input);
+        const playerInfoElement = this.createElement('div', {class: 'playerinfo visible'});
+        const player1Input = this.createElement('input', {type: 'text', id: 'name1'});
+        playerInfoElement.append(
+            this.createElement('label', {}, 'Player 1: '),
+            player1Input,
+            this.createElement('label', {}, 'Player 2: '),
+            this.createElement('input', {type: 'text', id: 'name2'}),
+            this.createElement('button', {id: 'pbtn'}, 'Submit'),
+        );
 
-        const player2Label = document.createElement('label');
-        player2Label.innerHTML = "Player 2: ";
-        const player2Input = document.createElement('input');
-        player2Input.setAttribute('type', 'text');
-        player2Input.id = 'name2';
-        playerInfoElement.appendChild(player2Label);
-        playerInfoElement.appendChild(player2Input);
+        const winningInfoElement = this.createElement('div', {class: 'winfo hidden'});
+        winningInfoElement.append(
+            this.createElement('h3', {id: 'p1'}),
+            this.createElement('h3', {id: 'p2'}),
+        );
 
-        const submitButton = document.createElement('button');
-        submitButton.textContent = 'Submit';
-        submitButton.id = 'pbtn';
-        playerInfoElement.appendChild(submitButton);
+        boardElements.append(playerInfoElement, winningInfoElement);
 
-        const winningInfoElement = document.createElement('div');
-        winningInfoElement.classList.add('winfo', 'hidden');
-        boardElements.appendChild(winningInfoElement);
+        document.getElementById('pbtn').addEventListener('click', () => this.startGame(playerInfoElement, winningInfoElement));
+        document.getElementById('reset').addEventListener('click', () => this.resetGame(playerInfoElement, winningInfoElement));
+    }
 
-        const player1Score = document.createElement('h3');
-        player1Score.id = 'p1';
-        const player2Score = document.createElement('h3');
-        player2Score.id = 'p2';
-        winningInfoElement.appendChild(player1Score);
-        winningInfoElement.appendChild(player2Score);
+    startGame(playerInfoElement, winningInfoElement) {
+        const playerOne = new Player(document.getElementById('name1').value, "red");
+        const playerTwo = new Player(document.getElementById('name2').value, "yellow");
+        this.players.push(playerOne, playerTwo);
 
-        submitButton.addEventListener('click', () => {
-            playerInfoElement.classList.remove('visible');
-            playerInfoElement.classList.add('hidden');
-            winningInfoElement.classList.remove('hidden');
-            winningInfoElement.classList.add('visible');
-            const playerOne = new Player(document.getElementById('name1').value, "red");
-            const playerTwo = new Player(document.getElementById('name2').value, "yellow");
-            player1Score.innerText = document.getElementById('name1').value + ": 0";
-            player1Score.style.color = 'red';
-            player2Score.innerText = document.getElementById('name2').value + ": 0";
-            player2Score.style.color = 'yellow';
-            players.push(playerOne, playerTwo);
-            const boardElements = document.querySelector('.board');
-            for (let row = 0; row < 6; row++) {
-                gameBoard[row] = [];
-                for (let col = 0; col < 7; col++) {
-                    const cell = document.createElement('div');
-                    cell.classList.add('cell', 'empty');
-                    cell.dataset.column = col;
-                    cell.dataset.row = row;
-                    gameBoard[row][col] = cell;
-                    boardElements.appendChild(cell);
-                }
+        this.updateVisibility(playerInfoElement, winningInfoElement);
+
+        this.updateScore(playerOne, playerTwo);
+
+        const boardElements = document.querySelector('.board');
+
+        for (let row = 0; row < 6; row++) {
+            this.gameBoard[row] = [];
+            for (let col = 0; col < 7; col++) {
+                const cell = this.createElement('div', {class: 'cell empty', 'data-column': col, 'data-row': row});
+                this.gameBoard[row][col] = cell;
+                boardElements.appendChild(cell);
             }
-            document.querySelectorAll('.cell').forEach((cell) => {
-                cell.addEventListener('click', () => {
-                    const column = parseInt(cell.dataset.column);
-                    handleCellClick(column);
-                });
-            });
-        });
+        }
 
-        document.getElementById('reset').addEventListener('click', () => {
-            const cells = document.querySelectorAll('.cell');
-            cells.forEach((cell) => {
-                cell.classList.remove('red', 'yellow');
-                cell.classList.add('empty');
+        document.querySelectorAll('.cell').forEach((cell) => {
+            cell.addEventListener('click', () => {
+                const column = parseInt(cell.dataset.column);
+                this.handleCellClick(column);
             });
-            gameBoard = [];
-            players = [];
-            const playerInfoElement = document.querySelector('.playerinfo');
-            playerInfoElement.classList.remove('hidden');
-            playerInfoElement.classList.add('visible');
-            const boardElements = document.querySelector('.board');
-            boardElements.innerHTML = "";
-            winningInfoElement.classList.remove('visible');
-            winningInfoElement.classList.add('hidden');
         });
     }
 
-    function checkWinCondition(row, col) {
-        function checkDirection(directionx, directiony) {
+    resetGame(playerInfoElement, winningInfoElement) {
+        const cells = document.querySelectorAll('.cell');
+        cells.forEach((cell) => {
+            cell.classList.remove('red', 'yellow');
+            cell.classList.add('empty');
+        });
+        this.gameBoard = [];
+        this.players = [];
+
+        this.updateVisibility(playerInfoElement, winningInfoElement);
+
+        const boardElements = document.querySelector('.board');
+        boardElements.innerHTML = "";
+    }
+
+    checkWinCondition(row, col) {
+        const checkDirection = (directionx, directiony) => {
             let count = 1;
             let rowWin = row + directionx;
             let colWin = col + directiony;
-            while (rowWin >= 0 && rowWin < 6 && colWin >= 0 && colWin < 7 && gameBoard[rowWin][colWin].classList.contains(players[currentPlayerIndex].color)) {
+            while (this.isWithinBounds(rowWin, colWin) && this.gameBoard[rowWin][colWin].classList.contains(this.players[this.currentPlayerIndex].color)) {
                 count++;
                 rowWin += directionx;
                 colWin += directiony;
@@ -112,7 +103,7 @@ const GameBoard = () => {
 
             rowWin = row - directionx;
             colWin = col - directiony;
-            while (rowWin >= 0 && rowWin < 6 && colWin >= 0 && colWin < 7 && gameBoard[rowWin][colWin].classList.contains(players[currentPlayerIndex].color)) {
+            while (this.isWithinBounds(rowWin, colWin) && this.gameBoard[rowWin][colWin].classList.contains(this.players[this.currentPlayerIndex].color)) {
                 count++;
                 rowWin -= directionx;
                 colWin -= directiony;
@@ -124,45 +115,47 @@ const GameBoard = () => {
         return (checkDirection(1, 0) || checkDirection(0, 1) || checkDirection(1, 1) || checkDirection(1, -1));
     }
 
-    function handleCellClick(column) {
+    handleCellClick(column) {
         for (let row = 5; row >= 0; row--) {
-            const cell = gameBoard[row][column];
+            const cell = this.gameBoard[row][column];
             if (cell.classList.contains('empty')) {
                 cell.classList.remove('empty');
-                cell.classList.add(players[currentPlayerIndex].color);
-                if (checkWinCondition(row, column)) {
+                cell.classList.add(this.players[this.currentPlayerIndex].color);
+                if (this.checkWinCondition(row, column)) {
                     setTimeout(() => {
-                        alert(`${players[currentPlayerIndex].name} wins!`);
-                        players[currentPlayerIndex].winnigs++;
-                        if(players[currentPlayerIndex].color === 'red') {
-                            const winner = document.getElementById('p1');
-                            winner.innerText = players[currentPlayerIndex].name + ": " + players[currentPlayerIndex].winnigs.toString();
-                        }
-                        else {
-                            const winner = document.getElementById('p2');
-                            winner.innerText = players[currentPlayerIndex].name + ": " + players[currentPlayerIndex].winnigs.toString();
-                        }
-                        const cells = document.querySelectorAll('.cell');
-                        cells.forEach((cell) => {
-                            cell.classList.remove('red', 'yellow');
-                            cell.classList.add('empty');
-                        });
+                        this.players[this.currentPlayerIndex].winnings++;
+                        this.updateScore(this.players[0], this.players[1]);
+                        this.resetGame(document.querySelector('.playerinfo'), document.querySelector('.winfo'));
                     }, 100);
                 } else {
-                    currentPlayerIndex = players[currentPlayerIndex].color === 'red' ? 1 : 0;
+                    this.currentPlayerIndex = this.players[this.currentPlayerIndex].color === 'red' ? 1 : 0;
                 }
                 return;
             }
         }
     }
 
-    return {
-        initializeGameBoard,
-    };
+    isWithinBounds(row, col) {
+        return row >= 0 && row < 6 && col >= 0 && col < 7;
+    }
 
+    updateVisibility(playerInfoElement, winningInfoElement) {
+        playerInfoElement.classList.toggle('visible');
+        playerInfoElement.classList.toggle('hidden');
+        winningInfoElement.classList.toggle('hidden');
+        winningInfoElement.classList.toggle('visible');
+    }
+
+    updateScore(player1, player2) {
+        const player1Score = document.getElementById('p1');
+        const player2Score = document.getElementById('p2');
+
+        player1Score.innerText = player1.name + ": " + player1.winnings;
+        player1Score.style.color = player1.color;
+
+        player2Score.innerText = player2.name + ": " + player2.winnings;
+        player2Score.style.color = player2.color;
+    }
 }
 
-
-
-const gameInstance = GameBoard();
-gameInstance.initializeGameBoard();
+const gameInstance = new GameBoard();
